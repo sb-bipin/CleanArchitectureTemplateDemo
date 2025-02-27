e872cb94b41bba8644977267122617e38c1d1746
sb-bipin <bipinpoudel406@gmail.com>
2025-02-27 22:07:45 +0545

    init

---
+++ Application/Interfaces/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<List<ProductDto>> GetAllAsync();
+        Task<ProductDto> GetByIdAsync(int id);
+        Task AddAsync(CreateProductDto product);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(UpdateProductDto product);
+
+    }
+}
---
+++ Application/Services/ProductService.cs
@@ -0,0 +1,49 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task AddAsync(CreateProductDto product)
+        {
+            await _productRepository.AddAsync(_mapper.Map<Product>(product));
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _productRepository.DeleteAsync(id);
+        }
+
+        public async Task<List<ProductDto>> GetAllAsync()
+        {
+            return _mapper.Map<List<ProductDto>>(await _productRepository.GetAllAsync());
+        }
+
+        public async Task<ProductDto> GetByIdAsync(int id)
+        {
+            return _mapper.Map<ProductDto>(await _productRepository.GetAsync(id));
+        }
+
+        public async Task UpdateAsync(UpdateProductDto product)
+        {
+            await _productRepository.UpdateAsync(_mapper.Map<Product>( product));
+        }
+    }
+}
